8e0cc14cca6b2bdc5c906acbe7fd8a2f2b1695dc
Mark Gunnels <markgunnels@gmail.com>
2012-02-09 04:42:53 -0500

    updated clj-http

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-loggly "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.2.3"]
+                 [clj-http "0.3.1"]
                  [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.3.4-SNAPSHOT"]])
